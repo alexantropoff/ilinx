2d8a43520603425852caeb73387da74ba30c71aa
Spy <spy@imac-spy.local>
2018-02-08 09:15:12 +0200

    Initial Commit

---
+++ main.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  iLinX
+//
+//  Created by mcf on 19/12/2008.
+//  Copyright Micropraxis Ltd 2008. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "iLinXAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain( argc, argv, nil, NSStringFromClass([iLinXAppDelegate class]));
+
+  [pool release];
+  return retVal;
+}
+
